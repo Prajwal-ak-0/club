93ab4235dad6ce4e9263247d98fd8ec0cbea0437
Prajwal A K <prajwalhassan7@gmail.com>
2024-02-12 23:18:06 +0530

    Desktop Navbar Completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,26 @@
 import Home from '@/components/Home'
+import { NavigationMenuDemo } from '@/components/Nav'
+import NewsTicker from '@/components/NewsTicker'
+import Logo from '@/components/navbar/Logo'
 import Navbar from '@/components/navbar/Navbar'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-      <div className="">
-        <Navbar/>
-      </div>
-      <Home/>
+      {/* <div className="">
+        <Navbar />
+      </div> */}
+      <div className="h-36 w-full bg-[#151515] flex items-center justify-start">
+            <div className='h-fit my-auto  w-fit mx-20'>
+                <Logo />
+            </div>
+            <div className="w-1/2 ml-64">
+                <NavigationMenuDemo />
+            </div>
+        </div>
+      <NewsTicker />
+      <Home />
     </div>
   )
 }
---
+++ components/NewsTicker.tsx
@@ -0,0 +1,14 @@
+// components/NewsTicker.tsx
+import React from 'react';
+
+const NewsTicker = () => {
+    return (
+        <div className="w-full bg-white/95 text-black h-12 flex items-center overflow-hidden">
+            <div className="whitespace-nowrap tracking-wide px-4 py-2 animate-marquee text-xl">
+            ** New article alert! **  &gt;&gt;&gt;  ** New article alert! **  &gt;&gt;&gt;  ** New article alert! **  &gt;&gt;&gt;  ** New article alert! **  &gt;&gt;&gt;  ** New article alert! **  &gt;&gt;&gt;  ** New article alert! **  &gt;&gt;&gt;
+            </div>
+        </div>
+    );
+};
+
+export default NewsTicker;
