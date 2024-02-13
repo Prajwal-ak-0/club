46b4721703bff766b9d38afea1ab5c1f6852bf45
Prajwal A K <prajwalhassan7@gmail.com>
2024-02-13 19:55:02 +0530

    Refactor Contact component layout

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -18,10 +18,6 @@ const Contact = () => {
                 <div className="absolute right-0 top-0 h-full w-1/2 bg-cover" style={{ backgroundImage: 'url(bg3.png)' }}></div>
                 <div className="relative w-1/2 z-10 text-white md:p-10 p-6">
                     <div className="mt-12 flex items-center justify-center md:gap-y-12 gap-y-8 md:mx-16 flex-col">
-{/*                        <Heading title="Contact" />
-                        <p className={`md:text-3xl text-2xl font-sans font-normal text-center md:leading-10 ${isHovered ? 'block' : 'hidden'}`}>
-                            Dive into the exciting world of AI/ML and its impact on engineering with the CI Club! Build &quot;cool stuff&quot; through hands-on projects and explore its potential across various fields. Join us and be part of the future!
-                        </p>*/}
                         <Heading title="Magazines & Newsletter" />
                         <p className={`md:text-3xl text-xl font-sans font-normal text-left md:leading-10 ${isHovered ? 'block' : 'hidden'}`}>
                             Dive into the cutting edge of technology with in-depth articles, expert insights and ground breaking discoveries in AI/ML.</p>
@@ -41,15 +37,15 @@ const Contact = () => {
             <div
                 onMouseEnter={() => setIsHovered(true)}
                 onMouseLeave={() => setIsHovered(false)}
-                className="relative min-h-screen bg-black"
+                className="relative min-h-screen md:hidden bg-black"
             >
-                <div className="absolute left-0 top-0 h-full w-full bg-cover opacity-10" style={{ backgroundImage: 'url(bg3.png)' }}>
-                
+                <div className="absolute left-0 top-0 h-full w-full bg-cover opacity-70" style={{ backgroundImage: 'url(bg3.png)' }}>
+
                 </div>
-                <div className="relative w-3/4 z-10 text-white md:p-10 py-6 pl- pr-4">
-                    <div className="mt-12 flex items-center mx-2 justify-center md:gap-y-12 gap-y-12 md:mx-16 flex-col">
-                        <Heading title="Activities and Events" />
-                        <p className={`md:text-3xl text-xl font-sans font-normal text-right md:leading-10 ${isHovered ? 'block' : 'hidden'}`}>
+                <div className="relative w-3/4 z-10 text-white md:p-10 py-6 pt-28 pl- pr-4">
+                    <div className="mt-12 flex items-center ml-6 justify-center md:gap-y-12 gap-y-12 md:mx-16 flex-col">
+                        <Heading title="Activities & Events" />
+                        <p className={`md:text-3xl text-2xl font-sans font-normal text-left md:leading-10 ${isHovered ? 'block' : 'hidden'}`}>
                             Discover a diverse range of learning opportunities, networking sessions and hands-on experiences</p>
                         <motion.div
                             initial={{ y: 0 }}
